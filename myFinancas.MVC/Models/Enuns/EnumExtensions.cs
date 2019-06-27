17118ccab1f6de2cbb7b8be7719ba97192a9f2af
Thiago Santiago <thiago.vps20@gmail.com>
2019-06-27 15:25:31 -0300

    Add classe cartão

---
+++ myFinancas.MVC/Models/Enuns/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace myFinancas.MVC.Models.Enuns
+{
+    public static class EnumExtensions
+    {
+        public static string TipoPontoToDescriptionString(this TipoBandeira val)
+        {
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+                .GetType()
+                .GetField(val.ToString())
+                .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+    }
+}
---
+++ myFinancas.MVC/Models/Enuns/TipoBandeira.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace myFinancas.MVC.Models.Enuns
+{
+    public enum TipoBandeira
+    {
+        [Description("Mastercard")]
+        MASTERCARD = 0,
+
+        [Description("Maestro")]
+        MAESTRO = 1,
+
+        [Description("Visa")]
+        VISA = 2
+    }
+}
