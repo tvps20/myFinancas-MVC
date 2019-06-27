2284c90f8a978fb7abc42b407bb8cfc4b9d19d38
Thiago Santiago <thiago.vps20@gmail.com>
2019-06-27 11:39:30 -0300

    Adicionando o template do dashboard

--- myFinancas.MVC/App_Start/BundleConfig.cs
+++ myFinancas.MVC/App_Start/BundleConfig.cs
@@ -7,7 +7,7 @@ namespace myFinancas.MVC
     {
         // Para obter mais informações sobre o agrupamento, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
-        {
+        {     
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -21,10 +21,40 @@ namespace myFinancas.MVC
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
-
+           
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            // CSS Files
+            bundles.Add(new StyleBundle("~/Content/assets/css/material-dashboard").Include(
+                "~/Content/assets/css/material-dashboard.css"));
+
+            // CSS Just for demo purpose, don't include it in your project
+            bundles.Add(new StyleBundle("~/Content/assets/demo-css").Include(
+                "~/Content/assets/demo/demo.css"));
+
+            // Core JS Files 
+            // ../assets/css/material-dashboard.css?v=2.1.0 versão retirada
+            bundles.Add(new ScriptBundle("~/Content/assets/js/core").Include(
+                "~/Content/assets/js/core/jquery.min.js",
+                "~/Content/assets/js/core/popper.min.js",
+                "~/Content/assets/js/core/bootstrap-material-design.min.js",
+                "~/Content/assets/js/plugins/perfect-scrollbar.jquery.min.js"));
+
+            // Chartist JS and Notifications Plugin
+            bundles.Add(new ScriptBundle("~/Content/js/plugins").Include(
+                "~/Content/assets/js/plugins/chartist.min.js",
+                "~/Content/assets/js/plugins/bootstrap-notify.js"));
+
+            // Control Center for Material Dashboard: parallax effects, scripts for the example pages etc
+            // ../assets/js/material-dashboard.js?v=2.1.0 versão retirada
+            bundles.Add(new ScriptBundle("~/Content/js/material-dashboard").Include(
+                "~/Content/assets/js/material-dashboard.js"));
+
+            // Material Dashboard DEMO methods, don't include it in your project!
+            bundles.Add(new StyleBundle("~/Content/assets/demo-js").Include(
+                "~/Content/assets/demo/demo.js"));
         }
     }
 }
