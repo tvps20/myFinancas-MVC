ae694371bb0c7056fb5fecead21ec9d01dfc60c6
Thiago Santiago <thiago.vps20@gmail.com>
2019-07-05 14:50:35 -0300

    Add modal de detalhes lancamento, não finalizado

---
+++ myFinancas.MVC/Controllers/LancamentoController.cs
@@ -0,0 +1,24 @@
+﻿using myFinancas.MVC.Models.Domain;
+using myFinancas.MVC.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myFinancas.MVC.Controllers
+{
+    public class LancamentoController : Controller
+    {
+        // GET: Lancamento
+        public ActionResult Detalhes(long id)
+        {
+            LancamentoModel Lancamento = new LancamentoModel();
+
+            if (id != 0) { LancamentoRepository.RecuperarPeloId(id); }
+            ViewBag.Lancamento = Lancamento;    
+            
+            return PartialView();
+        }
+    }
+}
