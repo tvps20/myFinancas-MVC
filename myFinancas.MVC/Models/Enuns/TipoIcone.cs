82aad9859f69d6e41abad4aba2797b32579c94c9
Thiago Santiago <thiago.vps20@gmail.com>
2019-07-06 11:05:36 -0300

    Refatorando icones

--- myFinancas.MVC/Models/Enuns/TipoIcone.cs
+++ myFinancas.MVC/Models/Enuns/TipoIcone.cs
@@ -13,6 +13,41 @@ namespace myFinancas.MVC.Models.Enuns
         [Description("error")]
         ERRO = 0,
         [Description("info")]
-        INFO = 1
+        INFO = 2,
+
+        [Description("payment")]
+        CARTAO = 3,
+        [Description("store")]
+        CONTAS = 4,
+        [Description("local_offer")]
+        NOTA = 5,
+        [Description("insert_drive_file")]
+        FATURA = 6,
+        [Description("access_time")]
+        TEMPO = 7,
+        [Description("cloud_download")]
+        DONWLOAD = 8,
+        [Description("shop")]
+        COMPRAS = 9,
+
+        [Description("edit")]
+        EDITAR = 10,
+        [Description("description")]
+        DETALHES = 11,
+        [Description("close")]
+        REMOVER = 12,
+
+        [Description("dashboard")]
+        DASHBOARD = 13,
+        [Description("payment")]
+        CARTOES = 14,
+        [Description("picture_as_pdf")]
+        FATURAS = 15,
+        [Description("people")]
+        COMPRADORES = 16,
+        [Description("shop_two")]
+        DIVIDAS = 17,
+        [Description("library_books")]
+        RELATORIOS = 18
     }
 }
